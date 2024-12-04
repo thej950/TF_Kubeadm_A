4a1a46a116f7607b2c0a3fc33f15eb35e7e836ad
thej <thej.devops01@gmail.com>
2024-12-04 22:07:51 +0530

    a

--- scripts/install_k8s_msr.sh
+++ scripts/install_k8s_msr.sh
@@ -1,95 +1,84 @@
 #!/bin/bash
 
-LOG_FILE="/var/log/k8s-setup.log"
-touch $LOG_FILE
-exec > >(tee -a $LOG_FILE) 2>&1
+######### ** FOR MASTER NODE ** #########
 
-echo "######## Starting Kubernetes Master Node Setup ########"
-
-# Set hostname
-echo "Setting hostname to k8s-Master"
-hostnamectl set-hostname k8s-Master
+hostname k8s-Master
 echo "k8s-Master" > /etc/hostname
 
-# Export AWS credentials
-echo "Exporting AWS credentials"
-export AWS_ACCESS_KEY_ID="${access_key}"
-export AWS_SECRET_ACCESS_KEY="${private_key}"
-export AWS_DEFAULT_REGION="${region}"
+export AWS_ACCESS_KEY_ID=${access_key}
+export AWS_SECRET_ACCESS_KEY=${private_key}
+export AWS_DEFAULT_REGION=${region}
 
-# Update system and install dependencies
-echo "Updating system and installing dependencies"
-apt update && apt install -y apt-transport-https ca-certificates curl software-properties-common
 
-# Add Docker repository
-echo "Adding Docker repository"
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+apt update
+apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 
-# Install Docker
-echo "Installing Docker"
-apt update && apt install -y docker-ce
+#Installing Docker
+apt update
+apt-cache policy docker-ce
+apt install docker-ce -y
+apt install awscli -y
+#Be sure to understand, if you follow official Kubernetes documentation, in Ubuntu 20 it does not work, that is why, I did modification to script
+#Adding Kubernetes repositories
 
-# Install AWS CLI
-echo "Installing AWS CLI"
-apt install -y awscli
+#Next 2 lines are different from official Kubernetes guide, but the way Kubernetes describe step does not work
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add
+echo "deb https://packages.cloud.google.com/apt kubernetes-xenial main" > /etc/apt/sources.list.d/kurbenetes.list
 
-# Add Kubernetes repository
-echo "Adding Kubernetes repository"
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-echo "deb https://packages.cloud.google.com/apt kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
-
-# Turn off swap
-echo "Disabling swap"
+#Turn off swap
 swapoff -a
 
-# Install Kubernetes tools
-echo "Installing Kubernetes tools (kubelet, kubeadm, kubectl)"
-apt update && apt install -y kubelet kubeadm kubectl
+#Installing Kubernetes tools
+apt update
+apt install kubelet kubeadm kubectl -y
 
-# Configure containerd
-echo "Configuring containerd"
+#next line is getting EC2 instance IP, for kubeadm to initiate cluster
+#we need to get EC2 internal IP address- default ENI is eth0
+export ipaddr=`ip address|grep eth0|grep inet|awk -F ' ' '{print $2}' |awk -F '/' '{print $1}'`
+export pubip=`dig +short myip.opendns.com @resolver1.opendns.com`
+
+# the kubeadm init won't work entel remove the containerd config and restart it.
 rm /etc/containerd/config.toml
 systemctl restart containerd
 
-# Get instance IP addresses
-echo "Getting EC2 instance IP addresses"
-ipaddr=$(ip address | grep eth0 | grep inet | awk '{print $2}' | awk -F'/' '{print $1}')
-pubip=$(dig +short myip.opendns.com @resolver1.opendns.com)
+#Kubernetes cluster init
+#You can replace 172.16.0.0/16 with your desired pod network
+kubeadm init --apiserver-advertise-address=$ipaddr --pod-network-cidr=172.16.0.0/16 --apiserver-cert-extra-sans=$pubip > /tmp/restult.out
+cat /tmp/restult.out
 
-# Initialize Kubernetes cluster
-echo "Initializing Kubernetes cluster"
-kubeadm init --apiserver-advertise-address="$ipaddr" --pod-network-cidr=172.16.0.0/16 --apiserver-cert-extra-sans="$pubip" | tee /tmp/result.out
+#to get join commdn
+tail -2 /tmp/restult.out > /tmp/join_command.sh;
+aws s3 cp /tmp/join_command.sh s3://${s3buckit_name};
+#this adds .kube/config for root account, run same for ubuntu user, if you need it
+mkdir -p /root/.kube;
+cp -i /etc/kubernetes/admin.conf /root/.kube/config;
+cp -i /etc/kubernetes/admin.conf /tmp/admin.conf;
+chmod 755 /tmp/admin.conf
 
-# Save join command
-echo "Saving join command"
-tail -2 /tmp/result.out > /tmp/join_command.sh
-aws s3 cp /tmp/join_command.sh s3://${s3buckit_name}
+#Add kube config to ubuntu user.
+mkdir -p /home/ubuntu/.kube;
+cp -i /etc/kubernetes/admin.conf /home/ubuntu/.kube/config;
+chmod 755 /home/ubuntu/.kube/config
 
-# Configure kubeconfig
-echo "Configuring kubeconfig"
-mkdir -p /root/.kube /home/ubuntu/.kube
-cp -i /etc/kubernetes/admin.conf /root/.kube/config
-cp -i /etc/kubernetes/admin.conf /home/ubuntu/.kube/config
-cp -i /etc/kubernetes/admin.conf /tmp/admin.conf
-chmod 755 /tmp/admin.conf /home/ubuntu/.kube/config /root/.kube/config
 
-# Apply Calico network
-echo "Applying Calico network"
+#to copy kube config file to s3
+# aws s3 cp /etc/kubernetes/admin.conf s3://${s3buckit_name}
+
+#Uncomment next line if you want calico Cluster Pod Network
 curl -o /root/calico.yaml https://docs.projectcalico.org/v3.16/manifests/calico.yaml
+sleep 5
 kubectl --kubeconfig /root/.kube/config apply -f /root/calico.yaml
-
-# Restart kubelet
-echo "Restarting kubelet"
 systemctl restart kubelet
 
-# Configure autocomplete and aliases for kubectl
-echo "Configuring autocomplete and aliases for kubectl"
-echo "source <(kubectl completion bash)" >> /home/ubuntu/.bashrc
-echo "source <(kubectl completion bash)" >> /root/.bashrc
+# Apply kubectl Cheat Sheet Autocomplete
+source <(kubectl completion bash) # set up autocomplete in bash into the current shell, bash-completion package should be installed first.
+echo "source <(kubectl completion bash)" >> /home/ubuntu/.bashrc # add autocomplete permanently to your bash shell.
+echo "source <(kubectl completion bash)" >> /root/.bashrc # add autocomplete permanently to your bash shell.
+alias k=kubectl
+complete -o default -F __start_kubectl k
 echo "alias k=kubectl" >> /home/ubuntu/.bashrc
 echo "alias k=kubectl" >> /root/.bashrc
 echo "complete -o default -F __start_kubectl k" >> /home/ubuntu/.bashrc
 echo "complete -o default -F __start_kubectl k" >> /root/.bashrc
-
-echo "######## Kubernetes Master Node Setup Complete ########"
